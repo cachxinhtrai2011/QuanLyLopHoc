daf2765757322bd7aefd27e849d0055e3990f6a6
kimquanghuy99 <18520411@gm.uit.edu.vn>
2021-12-01 13:22:26 +0700

    add

---
+++ DataLayer/InterFace/IAccountRepository.cs
@@ -0,0 +1,22 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer.InterFace
+{
+    public interface IAccountRepository
+    {
+        bool CheckuserName(string username);
+        GiaoVu LayThongtinGiaoVu(string TaiKhoan);
+        void DoiMatKhau(string MaGiaoVu, string MatKhauMoi);
+        int DoiThongTinTaiKhoan(string MaGiaoVU, string TenGiaoVu, string SDT, string Email, string TaiKhoan);
+        GiaoVu LayThongTinGiaoVuBangMaGiaoVu(string MaGiaoVu);
+        List<GiaoVu> LayDanhSachGiaoVu();
+        int ThemGiaoVu(string TenGiaoVu, string SDT, string Email, string TaiKhoan, string MatKhau);
+
+        
+    }
+}
