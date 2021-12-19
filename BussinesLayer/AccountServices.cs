d8705cd5cc16d28cc20e8e2f1e31a7f5c706a9af
2iamdobaongoc <94114289+2iamdobaongoc@users.noreply.github.com>
2021-12-19 15:53:21 +0700

    up

--- BussinesLayer/AccountServices.cs
+++ BussinesLayer/AccountServices.cs
@@ -10,7 +10,7 @@ using System.Threading.Tasks;
 namespace BussinesLayer
 {
     public static class AccountServices
-    {
+    { 
         static IAccountRepository AccRepo;
         static AccountServices()
         {
