e226701c186a7cbed0c66ef55b248b34908568a3
DoMinhPhucUIT <19522029@gm.uit.edu.vn>
2021-12-23 09:16:44 +0700

    Thêm thông tin

--- Model/HocSinh.cs
+++ Model/HocSinh.cs
@@ -21,6 +21,7 @@ namespace Model
         }
     
         public string MaHS { get; set; }
+        publuc string MaCMND {get; set; }
         public string TenHS { get; set; }
         public System.DateTime NgaySinh { get; set; }
         public Nullable<System.DateTime> NgayNhapHoc { get; set; }
